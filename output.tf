f36e00c4deecc169bf47b90ff031dc2f2af8f9e6
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-02-28 19:35:18 +0100

    update

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "resource_group_name" {
   value = azurerm_network_watcher.this.resource_group_name
 }
 
-output "resource_group_name" {
+output "id" {
   value = azurerm_network_watcher.this.id
 }
